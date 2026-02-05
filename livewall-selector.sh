a45f662d5427b5ea70fbf2be8604e87ed235503c
OkaPmna <okapramn092@gmail.com>
2026-02-06 01:53:28 +0800

    add disable livewallpp menu

--- livewall-selector.sh
+++ livewall-selector.sh
@@ -2,30 +2,34 @@
 
 # DIR Configuration
 VID_DIR="$HOME/Videos/livewall"
-
+DISABLE_STR="! DISABLE LIVE WALLPAPER"
 
 if [ ! -d "$VID_DIR" ]; then
-    notify-send "Error" "$VID_DIR not found."
+    notify-send "Error" "Directory $VID_DIR not found."
     exit 1
 fi
 
-# Get video list to wofi
-SELECTION=$(ls "$VID_DIR" | grep -E "\.(mp4|mkv|mov|webm)$" | wofi --dmenu --prompt "Pilih Live Wallpaper..." --width 400 --height 300)
+LIST=$(printf "%s\n%s" "$DISABLE_STR" "$(ls "$VID_DIR" | grep -E "\.(mp4|mkv|mov|webm)$")")
 
-# Selecting video file
-if [ -n "$SELECTION" ]; then
-    # Stop current livewallpaper
-    pkill mpvpaper
-    
-    # Start mpvpaper
-    mpvpaper -o "--hwdec=vaapi --vo=gpu --no-audio --override-display-fps=24 --loop" "*" "$VID_DIR/$SELECTION" &
-    # mpvpaper -o "no-audio --loop" "*" "$VID_DIR/$SELECTION" &
-    
+SELECTION=$(echo "$LIST" | wofi --dmenu --prompt "Select Live Wallpaper..." --width 400 --height 300 --cache-file /dev/null)
 
-    sleep 0.5
-    
-    # Reload Sway configuration
-    swaymsg reload
-    
-    notify-send "Wallpaper Diganti" "Sway direload & playing: $SELECTION"
+if [ -z "$SELECTION" ]; then
+    exit 0
 fi
+
+# Logic to turn off
+if [ "$SELECTION" = "$DISABLE_STR" ]; then
+    pkill mpvpaper
+    swaymsg reload
+    notify-send "Live Wallpaper" "Disabled"
+    exit 0
+fi
+
+# video selection
+pkill mpvpaper
+mpvpaper -o "--hwdec=vaapi --vo=gpu --no-audio --override-display-fps=24 --loop" "*" "$VID_DIR/$SELECTION" &
+
+sleep 0.5
+swaymsg reload
+
+notify-send "Wallpaper Updated" "Now playing: $SELECTION"
